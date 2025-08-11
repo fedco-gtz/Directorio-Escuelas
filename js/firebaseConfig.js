ce7531e6d3752c455bd49f3a17909a314abc4fef
fedco-gtz <gutierrezfedericog@gmail.com>
2025-08-11 19:59:59 -0300

    Actualización 11/08

---
+++ js/firebaseConfig.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBsEt6NEJ-NHpEw5YDK_JJvHXoEUgkMYiw",
+  authDomain: "colegios-6c66d.firebaseapp.com",
+  projectId: "colegios-6c66d",
+  storageBucket: "colegios-6c66d.firebasestorage.app",
+  messagingSenderId: "698954171487",
+  appId: "1:698954171487:web:6b71f3ca3a2d2c8a9687ea"
+};

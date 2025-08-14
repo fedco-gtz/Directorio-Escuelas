88ef4f1ddbac84ff71968754519402824b6e0bfc
fedco-gtz <gutierrezfedericog@gmail.com>
2025-08-13 23:41:33 -0300

    Actualización 13/08

--- js/app.js
+++ js/app.js
@@ -29,11 +29,31 @@ function renderizar(datos) {
   datos.forEach(e => {
     const card = document.createElement('div');
     card.className = 'school';
-card.innerHTML = `<strong>${e.nombre}</strong> · <strong style="background-color: red; color: white; padding: 3px 3px; border-radius: 5px;"> DESFAVORABILIDAD: ${e.desfavorabilidad || ''} </strong><div class="meta">${e.nivel} · ${e.barrio || e.direccion || ''}</div>`;
+
+    const desfavHTML = (e.desfavorabilidad && e.desfavorabilidad.toUpperCase() === 'SI')
+      ? `<strong style="background-color: red; color: white; padding: 3px; border-radius: 5px; display: inline-flex; justify-content: center; align-items: center;">D</strong>`
+      : '';
+
+    card.innerHTML = `
+      <strong>${e.nombre}</strong> ${desfavHTML}
+      <div class="meta">${e.nivel} · ${e.barrio || e.direccion || ''}</div>
+    `;
+
     lista.appendChild(card);
 
     if (e.lat && e.lng) {
-      const marker = L.marker([e.lat, e.lng]).bindPopup(`<strong>${e.nombre}</strong><br>${e.direccion || ''}`);
+      const marker = L.marker([e.lat, e.lng])
+.bindPopup(`
+  <strong>${e.nombre}</strong><br>
+  ${e.direccion || ''}<br>
+  ${
+    e.desfavorabilidad === 'SI'
+      ? `<strong style="background-color: red; color: white; padding: 3px; border-radius: 5px; display: inline-flex; justify-content: center; align-items: center;">
+           DESFAVORABILIDAD
+         </strong>`
+      : ''
+  }
+`);
       markers.addLayer(marker);
     }
   });
